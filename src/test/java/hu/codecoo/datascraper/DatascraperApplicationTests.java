44fa9435fdad1e20086966c69f4731d916d02644
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-24 09:20:51 +0200

    spring initializr

---
+++ src/test/java/hu/codecoo/datascraper/DatascraperApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.codecoo.datascraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatascraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
