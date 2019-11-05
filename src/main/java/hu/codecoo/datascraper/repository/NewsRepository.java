b3c81a6b95123e0e8c7e9470252f3905bd35d7fe
Taraszovics Péter <gtarapeti@gmail.com>
2019-11-05 14:42:54 +0100

    annotated interface as repository

--- src/main/java/hu/codecoo/datascraper/repository/NewsRepository.java
+++ src/main/java/hu/codecoo/datascraper/repository/NewsRepository.java
@@ -2,12 +2,13 @@ package hu.codecoo.datascraper.repository;
 
 import hu.codecoo.datascraper.entity.News;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NewsRepository extends CrudRepository<News, Integer> {
 
     News findById(int id);
     List<News> findAll();
-
 }
